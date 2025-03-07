195d274c3fc26e008d77dad14838c0b5fd8e9d95
Elkin Cardoso <elkin.cardoso@zip.co>
2025-03-07 12:29:35 -0500

    fix bug loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import ProductList from "./components/ProductList";
@@ -6,20 +5,9 @@ import AdminPanel from "./components/AdminPanel";
 import Cart from "./components/Cart";
 import ProtectedRoute from "./ProtectedRoute";
 import CheckoutForm from "./components/CheckoutForm";
-import { mockData } from "./constants/mockData";
 import { SnackbarProvider } from "notistack";
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const storedProducts = JSON.parse(localStorage.getItem("products") || "[]");
-    if (storedProducts.length === 0) {
-      const mockDataShort = mockData
-        .sort(() => 0.5 - Math.random())
-        .slice(0, 10);
-      localStorage.setItem("products", JSON.stringify(mockDataShort));
-    }
-  }, []);
-
   return (
     <SnackbarProvider maxSnack={3}>
       <Router>
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import { Product } from "../types/types";
 import "./ProductList.css";
+import { mockData } from "../constants/mockData";
 
-const ProductList: React.FC = () => {  
+const ProductList: React.FC = () => {
   const [storedProducts, setStoredProducts] = useState<Product[]>([]);
 
-  const storedProductsInLocalStorage = JSON.parse(localStorage.getItem("products") ?? "[]");
-
-  useEffect(() => {    
-    setStoredProducts(storedProductsInLocalStorage);
-  }, [storedProductsInLocalStorage]);
+  useEffect(() => {
+    const storedProducts = JSON.parse(localStorage.getItem("products") || "[]");
+    const mockDataShort = mockData.sort(() => 0.5 - Math.random()).slice(0, 10);
+    if (storedProducts.length === 0) {
+      localStorage.setItem("products", JSON.stringify(mockDataShort));
+    }
+    setStoredProducts(
+      storedProducts.length === 0 ? mockDataShort : storedProducts
+    );
+  }, []);
 
   return (
     <div className="product-list">
